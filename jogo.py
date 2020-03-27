a75e72b8085273c2e12a9d567741194fc4c3b03a
nataliaqmc <nataliaqmc@al.insper.edu.br>
2020-03-27 19:31:38 -0200

    quase o final

--- jogo.py
+++ jogo.py
@@ -21,25 +21,65 @@ while fichas != 0:
                 fichas = fichas - valor_aposta
             else:
                 print('Agora você está na fase Point:')
+                print('Você pode escolher entre quatro apostas, "Pass Line Bet", "Field", "Any Craps", ou "Twelve".')
+                aposta_escolhida = input('Qual aposta você fará?:')
                 Point = somadados
-                Point = True
                 dado3 = random.randint(1,6)
                 dado4 = random.randint(1,6)
                 somadados2 = dado3 + dado4
-                while Point:
-                    if somadados2 != Point and somadados2 != 7:
-                        print('Reiniciando fase Point: jogando os dados novamente!')
-                        dado3 = random.randint(1,6)
-                        dado4 = random.randint(1,6)
-                        somadados2 = dado3 + dado4
-                    elif somadados2 == Point:
+                if aposta_escolhida == 'Pass Line Bet':
+                    Point = True
+                    while Point:
+                        if somadados2 != Point and somadados2 != 7:
+                            print('Reiniciando fase Point: jogando os dados novamente!')
+                            dado3 = random.randint(1,6)
+                            dado4 = random.randint(1,6)
+                            somadados2 = dado3 + dado4
+                        elif somadados2 == Point:
+                            print('Você ganhou!')
+                            fichas = fichas + valor_aposta
+                            Point = False
+                        elif somadados2 == 7:
+                            print('Você perdeu!')
+                            fichas = 0
+                            Point = False
+                elif aposta_escolhida == 'Field' :
+                    dado1 = random.randint(1,6)
+                    dado2 = random.randint(1,6)
+                    somadados = dado1 + dado2
+                    if somadados == 5 or somadados == 6 or somadados == 7 or somadados == 8:
+                        print('Você perdeu tudo!')
+                        fichas = 0
+                    elif somadados == 2:
+                        print('Você ganhou fichas em dobro!')
+                        fichas = fichas + valor_aposta * 2
+                    elif somadados == 12:
+                        print('Você ganhou po triplo das fichas!')
+                        fichas = fichas + valor_aposta * 3
+                    else:
                         print('Você ganhou!')
                         fichas = fichas + valor_aposta
-                        Point = False
-                    elif somadados2 == 7:
+                elif aposta_escolhida == 'Any Craps':
+                    dado1 = random.randint(1,6)
+                    dado2 = random.randint(1,6)
+                    somadados = dado1 + dado2
+                    if somadados == 2 or somadados == 3 or somadados == 12:
+                        print('Você ganhou sete vezes o valor das fichas apostadas!')
+                        fichas = fichas + valor_aposta * 7
+                    else:
                         print('Você perdeu!')
-                        fichas = 0
-                        Point = False
+                        fichas = fichas - valor_aposta
+                elif aposta_escolhida == 'Twelve':
+                    dado1 = random.randint(1,6)
+                    dado2 = random.randint(1,6)
+                    somadados = dado1 + dado2
+                    if somadados == 12:
+                        print('Você ganhou 30 vezes o valor de fichas apostado!')
+                        fichas = fichas + valor_aposta * 30
+                    else:
+                        print('Você perdeu!')
+                        fichas = fichas - valor_aposta
+                
         elif aposta_escolhida == 'Field' :
             valor_aposta = int(input('Qual o valor da aposta?'))
             dado1 = random.randint(1,6)
@@ -79,7 +119,6 @@ while fichas != 0:
             else:
                 print('Você perdeu!')
                 fichas = fichas - valor_aposta
-        
     else:
         break
 print('GAME OVER!')
